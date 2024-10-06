9d8ee0a368c876bd6ff9b862c97b63168a9c2a23
Abrahamx9-R <68869264+Abrahamx9-R@users.noreply.github.com>
2024-10-06 11:11:46 -0600

    Update build hooks in start, core, testWorker, jest, webpack, and WebpackOptionsApply files

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,28 +3,37 @@ import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  private readonly API_KEY = 'bZQQyist6UgMcNUmdxHPQbWiunaxFAf0UlXboQXS'; // Replace with your actual API key
+  private readonly API_KEYS = [
+    'bZQQyist6UgMcNUmdxHPQbWiunaxFAf0UlXboQXS', // Reemplaza con tus API keys reales
+    'SBDEqwgg0Gsja11XYuYZHQUjfRH9bxzGNp59jXeY',
+    'xBOdYxfJmOvYiOWAhZ4ua6xQHSB7fcZkJIAx2QZ8',
+    'ObuJTjKs1WUQHAExJEnMoq7cr3r2ouTjcSVZ0V1d',
+    'A5aC8RQOhmwlQkuLz8bPNPcBgkSZiLdQCmFkR9YE',
+    'LzbE7zxB6n4jdeDHI4fCsBmRzvPGsypMHcfwkvdA'
+  ];
   private readonly BASE_URL = 'https://api.nasa.gov/neo/rest/v1';
+
+  private getRandomApiKey(): string {
+    const randomIndex = Math.floor(Math.random() * this.API_KEYS.length);
+    return this.API_KEYS[randomIndex];
+  }
 
   async getAllData(startDate: string): Promise<any[]> {
     const url = `${this.BASE_URL}/feed`;
     const params = {
-      api_key: this.API_KEY,
+      api_key: this.getRandomApiKey(),
       start_date: startDate,
       end_date: startDate,
     };
 
     try {
       const response = await axios.get(url, { params });
-      // console.log(response.data);
-      // console.log(url);
       const nearEarthObjects = response.data.near_earth_objects || {};
       let allData = [];
 
       for (const date in nearEarthObjects) {
         allData = allData.concat(nearEarthObjects[date]);
       }
-      // console.log(allData);
       return allData;
     } catch (error) {
       console.log(error);
@@ -42,15 +51,14 @@ export class AppService {
   async getOrbitalData(asteroidId: string): Promise<any> {
     const url = `${this.BASE_URL}/neo/${asteroidId}`;
     const params = {
-      api_key: this.API_KEY,
+      api_key: this.getRandomApiKey(),
     };
 
     try {
       const response = await axios.get(url, { params });
       console.log(response.data);
       console.log(url);
-      return response.data  || {};
-
+      return response.data || {};
     } catch (error) {
       if (error.response && error.response.status === 404) {
         throw new HttpException(
